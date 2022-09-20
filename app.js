7f67b00a83cc89458b3a4bfd1c2b0c0bd51e9f3e
saikatnoman12 <noommaa240@gmail.com>
2022-09-21 02:47:03 +0600

    add setter accessor. Lopping getter & setter accesor.

--- app.js
+++ app.js
@@ -1,20 +1,67 @@
-/*
+/*==========================================
 JavaScript introduce in two type Accessors:-
 1. Getter Accessors. Use get keyword.
 2. Setter Accessors. Use set keyword.
-*/ 
+==========================================*/
 
-// 1. Getter Accessor>
+/*== 
+1. Getter Accessor:-
+==*/
 const person = {
     firstName: 'Abdullah Al',
     lastName: 'Nomaan',
     language: 'Bangla',
     // getter accessor;
-    get fullName(){
+    get fullName() {
         // set full name accessor property value;
         return `${this.firstName} ${this.lastName}`;
     }
 }
 
 // check getter accessor output;
-console.log(person.fullName); // return output 'Abdullh Al Nomaan';
+console.log(person.fullName);   // return output 'Abdullh Al Nomaan';
+
+// lopping getter accessor and show object value;
+for(let i in person){
+    console.log(person[i]);
+    /* return:-
+    Abdullah Al
+    Nomaan
+    Bangla
+    Abdullah Al Nomaan
+    */
+}
+
+
+/*== 
+2. Setter Accessor:- 
+==*/
+const personTwo = {
+    firstName: 'Noman',
+    lastName: 'Abdullah',
+    age: 100,
+    // setter accessor;
+    set myAge(year) {
+        this.age = year;
+
+        return this.age;
+    }
+}
+
+// set value in age parameter; 
+personTwo.myAge = 30;
+// check setter accessor output;
+console.log(personTwo.age); // return output '30';
+
+// lopping setter accessor, and show object value;
+for(let i in personTwo){
+    console.log(personTwo[i]);
+    /* return:-
+    Nomaan
+    Abdullah
+    30
+    undefined
+    */
+}
+
+
